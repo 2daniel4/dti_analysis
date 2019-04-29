a7f7093b7ba8b921f0f5be8a19379d7488ed50cf
2daniel4 <33704076+2daniel4@users.noreply.github.com>
2019-04-29 09:38:36 -0400

    Update trac-all-prep-long.sh

--- trac-all-prep-long.sh
+++ trac-all-prep-long.sh
@@ -4,19 +4,14 @@ setenv FREESURFER_HOME /Applications/freesurfer/
 source $FREESURFER_HOME/SetUpFreeSurfer.csh
 #Must set SUBJECTS_DIR to where data is or else uses what was set up when installing freesurfer
 setenv SUBJECTS_DIR /Volumes/DANIEL/dti_freesurf/diffusion_recons/
+#set path to configuration file
+setenv config /Volumes/DANIEL/
 @ linenum = 1
 
-#Create directories for trac-all (Should be indexing into folders with recon-all and dti dicoms)
-
-
-#cd /Volumes/DANIEL/
-#mkdir dti_freesurf
-#cd /Volumes/DANIEL/dti_freesurf
-#mkdir diffusion_recons
-#cd /Volumes/DANIEL/
+#Make sure to create directories for trac-all (Should be indexing into folders with recon-all and dti dicoms) that are specified in config file
 
 #Run trac-all -prep version which includes, eddy and b0 corrrection and registration to anat.
 #Calls on configuration file
 
-#trac-all -prep -jobs file -c /Volumes/DANIEL/dmrirc.long.example.sh to create jobs files
-trac-all -prep -c /Volumes/DANIEL/dmrirc.long.example.sh
+#trac-all -prep -jobs file -c ${config}/dmrirc.long.example.sh to create jobs files
+trac-all -prep -c ${config}/dmrirc.long.example.sh
