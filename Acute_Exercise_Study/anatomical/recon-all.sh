be3ed6dd6c0a200c102cba64b620aae0f9eb5350
Daniel Callow <33704076+CallowBrainProject@users.noreply.github.com>
2019-10-02 07:50:39 -0400

    Update recon-all.sh

--- Acute_Exercise_Study/anatomical/recon-all.sh
+++ Acute_Exercise_Study/anatomical/recon-all.sh
@@ -29,7 +29,29 @@ foreach cond (Ex Rest)
 echo "subj = "${subj}", cond = " ${cond}
 
 #must have Dicoms folder created. Or some folder with a path to anatomical Dicom images. Input first dicom and will load all of them. subjid gives the naming of each subject
-recon-all -all -hippocampal-subfields-T1 -i DICOMS/${subj}/session-${cond}/anat/image000001.dcm -subjid ${subj}_${cond} 
+recon-all -all -base ${subj} ${con} -i /Volumes/DANIEL/Dicoms/${subj}/session-${cond}/anat/image000001.dcm
 
 end
 end
+
+#For longitudinal processing must have done cross-sectional processing first
+
+foreach subj (AES101 AES102 AES104 AES107 AES108 AES109 AES110 AES111 AES113 AES114 AES115 AES116 AES117 AES118 AES119 AES120 AES121 AES122 AES123 AES124 AES126 AES127 AES129 AES130 AES133 AES134 AES136 AES137 AES138 AES139)
+
+foreach cond (Ex Rest)
+
+recon-all -base  base_${subj} -tp Ex.${subj} -tp Rest.${subj} -all
+
+recon-all -long ${cond}.${subj} base_${subj} -all
+
+end
+end
+
+
+# To segment hippocampus with freesurfer dev version
+foreach subj (AES101 AES102 AES104 AES107 AES108 AES109 AES110 AES111 AES113 AES114 AES115 AES116 AES117 AES118 AES119 AES120 AES121 AES122 AES123 AES124 AES126 AES127 AES129 AES130 AES133 AES134 AES136 AES137 AES138 AES139)
+
+segmentHA_T1_long.sh base_${subj}
+
+end
+
