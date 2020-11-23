670ad4f4c2e36f83ce15bcc53b0cd7b123f212ef
Daniel Callow <33704076+CallowBrainProject@users.noreply.github.com>
2020-11-23 16:00:59 -0500

    Create mrtrix_.sh

---
+++ bswift_mrtrix3/development_paper/mrtrix_.sh
@@ -0,0 +1,31 @@
+#!/bin/tcsh -f
+#SBATCH --time=300:00:00
+#SBATCH --mem=24000
+#SBATCH --output=results
+#SBATCH --cores=6
+
+# The above is required any time you are submitting to SLURM (job management software) on bswift. 
+# Line 1 indicates we are using tcsh and the -f indicates we will be supplying arguments (subject ID's)
+# Line 2 - maximum amount of time allocated
+# Line 3 - memory requested
+# Line 4 - name of file with command output
+# Line 5 - the number of cores/nodes requested
+
+# Set paths to mrtrix and ANTS software.
+set path = ( $path /data/bswift-0/software/ANTs-2019-11/MRtrix3Tissue/bin)
+set path = ( $path /data/bswift-0/software/ANTs-2019-11/ANTs-2.1.0-Linux/bin)
+
+#Set subjects directory (Where you will access stored data)
+setenv SUBJECTS_DIR /data/bswift-1/dcallow
+
+# Echo commands/output
+set echo
+# sets the variable ${subj} equal to the input value we provide (see the above note about the -f in shebang)
+set subj= ($argv[1]) \
+
+# Make directory for each subject and a dwi folder for the analysis
+mkdir SUBJECTS_DIR/${subj}/
+mkdir SUBJECTS_DIR/${subj}/dwi
+
+# Go to dwi folder (will do all processing here)
+cd SUBJECTS_DIR/${subj}/dwi
