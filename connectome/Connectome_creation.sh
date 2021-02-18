b7b8b9b609f954db146af77c92713f0e58279f68
Daniel Callow <33704076+CallowBrainProject@users.noreply.github.com>
2021-02-18 12:03:25 -0500

    Update Connectome_creation.sh

--- connectome/Connectome_creation.sh
+++ connectome/Connectome_creation.sh
@@ -29,10 +29,24 @@ source ${FSLDIR}/etc/fslconf/fsl.csh
 
 cd ${SUBJECTS_DIR}/CONNECTOME/dwi/${subj}/T1w/
 
+# Create an image 5TT.mif with the different tissue type segmentations
 5ttgen freesurfer -force ${SUBJECTS_DIR}/CONNECTOME/dwi/${subj}/T1w/Diffusion/cort_subcort.nii.gz 5TT.mif
+
+# Create vis.mif for visualization purposes
 5tt2vis -force 5TT.mif vis.mif
+
+#extract mean bo
+mrconvert -force ${SUBJECTS_DIR}/CONNECTOME/dwi/${subj}/T1w/Diffusion/wmfod.mif - -coord 3 0 | mrcat ${SUBJECTS_DIR}/CONNECTOME/dwi/${subj}/T1w/Diffusion/csf.mif ${SUBJECTS_DIR}/CONNECTOME/dwi/${subj}/T1w/Diffusion/gm.mif - tissueRGB.mif -axis 3
+
+# Take freesurfer subcortical segmentation and convert it into nodes for tractography and connectome analysis 
 labelconvert -force ${SUBJECTS_DIR}/CONNECTOME/dwi/${subj}/T1w/Diffusion/cort_subcort.nii.gz /data/bswift-1/dcallow/CONNECTOME/FreeSurferColorLUT.txt /data/bswift-1/dcallow/CONNECTOME/fs_default.txt nodes.mif
-tck2connectome -force 1M_SIFT.tck nodes.mif connectome.csv -symmetric -zero_diagonal
-tck2connectome -force 1M_SIFT.tck nodes.mif distance.csv -scale_length -stat_edge mean -symmetric -zero_diagonal
-tcksample -force 1M_SIFT.tck ${SUBJECTS_DIR}/CONNECTOME/dwi/${subj}/T1w/Diffusion/FA.nii.gz mean_FA_per_streamline.csv -stat_tck mean
-tck2connectome -force 1M_SIFT.tck nodes.mif mean_FA_connectome.csv -scale_file mean_FA_per_streamline.csv -stat_edge mean -symmetric -zero_diack2connectome -force 1M_SIFT.tck nodes.mif mean_FA_connectome.csv -scale_file mean_FA_per_streamline.csv -stat_edge mean -symmetric -zero_diagonal
+
+
+# Perform stractography of 10 Million streamlines using act
+tckgen -force ${SUBJECTS_DIR}/CONNECTOME/dwi/${subj}/T1w/Diffusion/wmfod.mif 10M.tck -act 5TT.mif -backtrack -crop_at_gmwmi -seed_dynamic ${SUBJECTS_DIR}/CONNECTOME/dwi/${subj}/T1w/Diffusion/wmfod.mif -maxlength 250 -select 10M -step 1 -angle 45 -cutoff 0.06
+
+# Filter and get weights from tcksift2
+tcksift2 -force 10M.tck ${SUBJECTS_DIR}/CONNECTOME/dwi/${subj}/T1w/Diffusion/wmfod.mif -act 5TT.mif weights.csv
+
+# Create connectome from tck using above calculated weights
+tck2connectome -force 10M.tck nodes.mif connectome.csv -tck_weights_in weights.csv -out_assignments assignments.txt -symmetric -zero_diagonal
